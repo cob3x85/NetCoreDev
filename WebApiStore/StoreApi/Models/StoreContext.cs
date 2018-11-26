367bfef75468e60d1d2a1af13e6f8a9ada101658
Carlos Ortiz <carlos.ortiz@crownpeak.com>
2018-11-25 21:20:39 -0600

    Initial Web Api settings

---
+++ WebApiStore/StoreApi/Models/StoreContext.cs
@@ -0,0 +1,18 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace StoreApi
+{
+
+  public class StoreContext : DbContext
+  {
+    public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+    {
+
+    }
+
+    // Add the Entities Info
+    public DbSet<User> Users { get; set; }
+  }
+
+}
---
+++ WebApiStore/StoreApi/Models/Users.cs
@@ -0,0 +1,10 @@
+namespace StoreApi
+{
+  public class User
+  {
+    public string Description { get; set; }
+    public string Email { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+  }
+}
